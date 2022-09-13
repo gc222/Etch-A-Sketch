f062af25998e644a7b9d5124422fb2fe89da2ca5
Gregory Chung <gregorychung21@gmail.com>
2022-09-13 14:23:46 +0100

    added controls and grid size change

--- script.js
+++ script.js
@@ -1,22 +1,91 @@
+// globals
 const gridContainer = document.getElementById("grid-container");
+const colourPicker = document.getElementById("colour-picker");
+const paintBrush = document.getElementById("paint-brush");
+const eraser = document.getElementById("eraser");
+const clear = document.getElementById("clear");
+const sliderValue = document.getElementById("slider-value");
+const slider = document.getElementById("slider");
+const gridLines = document.getElementById("grid-lines");
 
-const length = 16
-const gridSize = length * length
+let draw = true;
+let colour = "black";
 
-for ( let i = 0; i < gridSize; i++ ) {
-    let cell = document.createElement("div");
+function setupGrid(length) {
+    // if gridContainer has children, remove all before changing grid size
+    // textContent represents the text and also its DESCENDANTS nodes
+    if (gridContainer.hasChildNodes()) {
+        gridContainer.textContent = "";     // remove the (cell) child nodes 
+    }
 
-    cell.classList.add("grid-item");    // add class
+    let gridSize = length * length;
+    
+    for ( let i = 0; i < gridSize; i++ ) {
+        let cell = document.createElement("div");
+    
+        cell.classList.add("grid-item");    // add class
+    
+        gridContainer.appendChild(cell);
+        
+        // draw
+        cell.addEventListener("mouseover", () => {
+            if (draw) {
+                cell.style.background = colour;
+            } else {
+                cell.style.background = "white";
+            }
+        });
 
-    gridContainer.appendChild(cell);
+        // cell.addEventListener("mouseout", () => {
+        //     cell.style.background = "white";
+        // });
+        
+    }
 
-    cell.addEventListener("mouseover", () => {
-        cell.style.background = "yellow";
-        console.log("change-colour");
-    });
-
-    cell.addEventListener("mouseout", () => {
-        // cell.style.background = "white";
-    });
+    // grid-template-columns: repeat(16, 1fr)
+    gridContainer.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
 }
 
+// colour picker
+colourPicker.addEventListener("change", () => {
+    colour = colourPicker.value;
+})
+
+// black ink
+paintBrush.addEventListener("click", () => draw = true);
+
+// eraser
+eraser.addEventListener("click", () => draw = false);
+
+// grid lines
+gridLines.addEventListener("click", () => {
+    let cellNodeList = gridContainer.children;
+    cellList = [...cellNodeList];   // use spread notation to change to array
+    
+    cellList.forEach(cell => {
+        if (cell.style.outline) {
+            cell.style.outline = null; 
+        } else {
+            cell.style.outline =  "1px solid grey";
+        }
+    });  
+}); 
+
+// clear grid
+clear.addEventListener("click", () => {
+    setupGrid(slider.value);
+});
+
+// change grid size on slider event
+slider.addEventListener("change", () => {
+    setupGrid(slider.value);    // pass value attribute
+
+    // update slider value
+    sliderValue.textContent = `Grid size: ${slider.value} x ${slider.value}`;    
+});
+
+
+// initial setup
+setupGrid(16);
+// run once to display slider value
+sliderValue.textContent = `Grid size: ${slider.value} x ${slider.value}`;    
